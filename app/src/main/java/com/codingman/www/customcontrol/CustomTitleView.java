726552d36853abcf83e9ccc82c2df49c36616543
wsxingjun <1185889308@qq.com>
2018-06-08 15:01:35 +0800

    增加了wrap_content 的约束，随机数的参考设置；

--- app/src/main/java/com/codingman/www/customcontrol/CustomTitleView.java
+++ app/src/main/java/com/codingman/www/customcontrol/CustomTitleView.java
@@ -10,12 +10,15 @@ import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 /**
  * @function
  */
 
-public class CustomTitleView extends View{
+public class CustomTitleView extends View {
     private String mTitleText;
     private int mTitleTextColor;
     private int mTitleTextSize;
@@ -23,32 +26,34 @@ public class CustomTitleView extends View{
     //绘制时控制文本的绘制范围；
     private Rect mBound;
     private Paint mPaint;
+    private Set<Integer> hashSet;
 
     public CustomTitleView(Context context, AttributeSet attr) {
-        this(context,attr,0);
+        this(context, attr, 0);
     }
 
     public CustomTitleView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     /**
      * 获取自定义的样式属性
+     *
      * @param context
      * @param attrs
      * @param defStyle
      */
-    public CustomTitleView(Context context,AttributeSet attrs,int defStyle) {
-        super(context,attrs,defStyle);
+    public CustomTitleView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
 
-        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs,R.styleable., defStyle, 0);
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomTitleView, defStyle, 0);
         int n = typedArray.getIndexCount();
-        for (int i=0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             int attr = typedArray.getIndex(i);
-            switch(attr){
+            switch (attr) {
                 case R.styleable.CustomTitleView_titleText:
                     mTitleText = typedArray.getString(attr);
-                break;
+                    break;
 
                 case R.styleable.CustomTitleView_titleTextColor:
                     //default color is Back;
@@ -57,7 +62,7 @@ public class CustomTitleView extends View{
 
                 case R.styleable.CustomTitleView_titleTextSize:
                     mTitleTextSize = typedArray.getDimensionPixelSize(attr,
-                            (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,16,getResources().getDisplayMetrics()));
+                            (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
                     break;
             }
         }
@@ -67,21 +72,85 @@ public class CustomTitleView extends View{
         mPaint = new Paint();
         mPaint.setTextSize(mTitleTextSize);
         mBound = new Rect();
-        mPaint.getTextBounds(mTitleText,0,mTitleText.length(),mBound);
+        mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+
+        this.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTitleText = randomText();
+                postInvalidate();
+            }
+
+
+        });
+    }
+
+    private String randomText() {
+        Random random = new Random();
+        hashSet = new HashSet<>();
+        while (hashSet.size() < 4){
+            int randomInt = random.nextInt(10);
+            hashSet.add(randomInt);
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (Integer i: hashSet){
+            sb.append("" + i);
+        }
+
+        return sb.toString();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+
+
+        int width = 0;
+        int height = 0;
+
+        switch (widthMode) {
+            case MeasureSpec.EXACTLY:
+                width = getPaddingLeft() + getPaddingRight() + widthSize;
+
+                break;
+            case MeasureSpec.AT_MOST:
+                width = getPaddingLeft() + getPaddingRight() + mBound.width();
+                break;
+            default:
+                break;
+
+        }
+
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        switch (heightMode) {
+            case MeasureSpec.EXACTLY:
+                height = getPaddingTop() + getPaddingBottom() + heightSize;
+
+                break;
+            case MeasureSpec.AT_MOST:
+                height = getPaddingTop() + getPaddingBottom() + mBound.height();
+                break;
+            default:
+                break;
+
+        }
+
+        setMeasuredDimension(width, height);
     }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         mPaint.setColor(Color.YELLOW);
-        canvas.drawRect(0,0,getMeasuredWidth(),getMeasuredHeight(),mPaint);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
 
         mPaint.setColor(mTitleTextColor);
-        canvas.drawText(mTitleText,getWidth()/2-mBound.width()/2,(getHeight() + mBound.height())/2,mPaint);
+        canvas.drawText(mTitleText, getWidth() / 2 - mBound.width() / 2, getHeight()/2 + mBound.height()/ 2, mPaint);
 
         super.onDraw(canvas);
     }
