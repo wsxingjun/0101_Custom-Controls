69af29b161ddd3763419963d9d1da27726af9fc4
wsxingjun <1185889308@qq.com>
2018-06-15 16:04:55 +0800

    出现运行错误bug：Error:Execution failed for task ':customviewvisibleview1:mergeDebugResources'. > -1

--- customviewvisibleview1/src/main/java/com/oztaking/www/customviewvisibleview1/MyViewQQDeleteDemo15.java
+++ customviewvisibleview1/src/main/java/com/oztaking/www/customviewvisibleview1/MyViewQQDeleteDemo15.java
@@ -5,26 +5,38 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * @function
  */
 
-public class MyViewQQDeleteDemo15 extends FrameLayout{
+public class MyViewQQDeleteDemo15 extends FrameLayout {
 
     private Paint mPaint;
     private Path mPath;
 
 
-    private Ponit mCurPoint,mStartPoint;
-    private int mRadius = 20;
+    private Ponit mCurPoint, mStartPoint;
+
+    private final int DEFAULT_RADIUS = 20;
+    private int mRadius = DEFAULT_RADIUS;
 
     private boolean mTouch;
+
+    private TextView mTextView;
+    private ImageView mImageView;
+    private boolean isAnimStart;
 
 
     public MyViewQQDeleteDemo15(Context context) {
@@ -41,38 +53,85 @@ public class MyViewQQDeleteDemo15 extends FrameLayout{
 
 
     private void initView() {
-        mStartPoint = new Ponit(100,100);
+        mStartPoint = new Ponit(100, 100);
         mCurPoint = new Ponit();
         mPaint = new Paint();
         mPaint.setColor(Color.RED);
         mPaint.setStyle(Paint.Style.FILL);
 
         mPath = new Path();
+
+        mTextView = createTextView();
+        mImageView = createImageView();
     }
 
     @Override
     protected void dispatchDraw(Canvas canvas) {
         //新建图层
-        canvas.saveLayer(new RectF(0,0,getWidth(),getHeight()),mPaint,Canvas.ALL_SAVE_FLAG);
+        canvas.saveLayer(new RectF(0, 0, getWidth(), getHeight()), mPaint, Canvas.ALL_SAVE_FLAG);
         //图层画圆
-        canvas.drawCircle(mStartPoint.x,mStartPoint.y,mRadius,mPaint);
-        //如果检测到手指的触摸，画出当前的圆
-        if (mTouch){
+
+        //手没有接触或者动画开始的时候
+        if (!mTouch || isAnimStart) {
+
+            //绘制textView在起始位置；
+            mTextView.setX((mStartPoint.x - mTextView.getWidth() / 2));
+            mTextView.setY(mStartPoint.y - mTextView.getHeight() / 2);
+
+        }else {
+            //如果检测到手指的触摸，画出当前的textview；
             calculatePath();
-            canvas.drawCircle(mCurPoint.x,mCurPoint.y,mRadius,mPaint);
-            canvas.drawPath(mPath,mPaint);
+            canvas.drawCircle(mCurPoint.x, mCurPoint.y, mRadius, mPaint);
+            canvas.drawCircle(mStartPoint.x, mStartPoint.y, mRadius, mPaint);
+
+            canvas.drawPath(mPath, mPaint);
+
+            //将textView的中心位置放到手的位置；
+            mTextView.setX((mCurPoint.x - mTextView.getWidth() / 2));
+            mTextView.setY(mCurPoint.y - mTextView.getHeight() / 2);
+
         }
         canvas.restore();
         super.dispatchDraw(canvas);
     }
 
+    private TextView createTextView() {
+        TextView textView = new TextView(getContext());
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        textView.setLayoutParams(params);
+        textView.setPadding(10, 10, 10, 10);
+        textView.setTextSize(20);
+        textView.setTextColor(Color.CYAN);
+        textView.setBackgroundResource(R.drawable.textbg);
+        textView.setText("拖我");
+        addView(textView);
+        return textView;
+    }
+
+    private ImageView createImageView(){
+        ImageView imag = new ImageView(getContext());
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        imag.setLayoutParams(params);
+        imag.setImageResource(R.drawable.imagebobo);
+        imag.setVisibility(INVISIBLE);
+        addView(imag);
+        return imag;
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
-        switch(event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                mTouch = true;
-            break;
+
+                boolean isTextViewTouch = isTouchRect(mTextView, event);
+                if (isTextViewTouch) {
+                    mTouch = true;
+                }
+
+                break;
 
             case MotionEvent.ACTION_MOVE:
 
@@ -83,24 +142,56 @@ public class MyViewQQDeleteDemo15 extends FrameLayout{
                 break;
         }
 
-        mCurPoint.setX(event.getX(),event.getY());
+        mCurPoint.setX(event.getX(), event.getY());
         postInvalidate();
         return true;
     }
 
+    private boolean isTouchRect(TextView textView, MotionEvent event) {
+        Rect rect = new Rect();
+        int[] location = new int[2];
+        //获取当前控件所在屏幕的位置，传进去一个location的数组，在执行以后会把left,top值赋给location[0]和location[1]
+        textView.getLocationOnScreen(location);
+
+        rect.left = location[0];
+        rect.top = location[1];
+        rect.right = rect.left + textView.getWidth();
+        rect.bottom = rect.top + textView.getHeight();
+
+        if (rect.contains((int) event.getRawX(), (int) event.getRawY())) {
+            return true;
+        }
+        return false;
+    }
+
     //计算贝塞尔的路径
-    private void calculatePath(){
+    private void calculatePath() {
         float x = mCurPoint.x;
         float y = mCurPoint.y;
 
         float startX = mStartPoint.x;
         float startY = mStartPoint.y;
 
+        //根据距离变化圆的半径；
+        float distance = (float) Math.sqrt(Math.pow(y-startY,2) + Math.pow(x - startX,2));
+        mRadius = (int) (DEFAULT_RADIUS - distance/5);
+        if (mRadius < 9){
+            isAnimStart = true;
+            mImageView.setX(mCurPoint.x - mTextView.getWidth()/2);
+            mImageView.setY(mCurPoint.y - mTextView.getHeight()/2);
+            mImageView.setVisibility(VISIBLE);
+            //直接强转，然后开始动画；
+            ((AnimationDrawable)mImageView.getDrawable()).start();
+            mTextView.setVisibility(GONE);
+//            mRadius = 9;
+
+        }
+
         //根据角度计算出四边形的四个点；
         float dx = x - startX;
         float dy = y - startY;
 
-        double a = Math.atan(dy/dx);
+        double a = Math.atan(dy / dx);
         float offsetX = (float) (mRadius * Math.sin(a));
         float offsetY = (float) (mRadius * Math.cos(a));
 
@@ -116,20 +207,17 @@ public class MyViewQQDeleteDemo15 extends FrameLayout{
         float x3 = startX - offsetX;
         float y3 = startY + offsetY;
 
-        float anchorX = (startX + x)/2;
-        float anchorY = (startY + y)/2;
+        float anchorX = (startX + x) / 2;
+        float anchorY = (startY + y) / 2;
 
         //连线成为一个封闭的图形；
         mPath.reset();
-        mPath.moveTo(x0,y0);
-        mPath.quadTo(anchorX,anchorY,x1,y1);
-        mPath.lineTo(x2,y2);
-        mPath.quadTo(anchorX,anchorY,x3,y3);
-        mPath.lineTo(x0,y0);
+        mPath.moveTo(x0, y0);
+        mPath.quadTo(anchorX, anchorY, x1, y1);
+        mPath.lineTo(x2, y2);
+        mPath.quadTo(anchorX, anchorY, x3, y3);
+        mPath.lineTo(x0, y0);
     }
-
-
-
 
 
     private class Ponit {
@@ -144,7 +232,7 @@ public class MyViewQQDeleteDemo15 extends FrameLayout{
             this.y = y;
         }
 
-        public void setX(float x,float y) {
+        public void setX(float x, float y) {
             this.x = x;
             this.y = y;
         }
