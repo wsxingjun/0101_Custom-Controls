68789eff67bb6e3541887ede2bf32cc1d09796b4
wsxingjun <1185889308@qq.com>
2018-06-15 16:44:05 +0800

    android studio 运行莫名出现错误

--- customviewvisibleview1/src/main/java/com/oztaking/www/customviewvisibleview1/MainActivity.java
+++ customviewvisibleview1/src/main/java/com/oztaking/www/customviewvisibleview1/MainActivity.java
@@ -2,6 +2,7 @@ package com.oztaking.www.customviewvisibleview1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
